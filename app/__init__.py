545d0dd205ea982a2d82e4c673dbf9e3b79183c7
kbrudnowski <krzysztof.brudnowski@gmail.com>
2023-08-31 20:56:02 +0200

    upload file endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_dropzone import Dropzone
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,26 @@
 from app import app
+from flask import render_template, request, redirect, flash, url_for
+from werkzeug.utils import secure_filename
 
-@app.route('/')
-def index():
-    return 'Hello, Flask!'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            return 'Well done'
