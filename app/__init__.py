a894962cfd61eb93be5cf610bbb1fe2fd8e75583
kbrudnowski <krzysztof.brudnowski@gmail.com>
2023-09-04 15:08:45 +0200

    dropzone config

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
-from flask_dropzone import Dropzone
 
 app = Flask(__name__)
-app.debug = True
-
-app.config['DROPZONE_MAX_FILES'] = 1
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image'
-app.config['DROPZONE_MAX_FILE_SIZE'] = 5
-
-dropzone = Dropzone(app)
 
 from app import routes
